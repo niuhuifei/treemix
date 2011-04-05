37384323ce78d992269e36681dfd1bbcc9ad6b88
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-04-05 23:48:45 +0000

    small changes to MCMC class

--- trunk/MCMC.cpp
+++ trunk/MCMC.cpp
@@ -6,3 +6,23 @@
  */
 
 #include "MCMC.h"
+
+MCMC::MCMC(State* s, MCMC_params* p){
+	state = s;
+	param =p;
+}
+
+
+void MCMC::run(gsl_rng* r){
+	cout <<"Burn in of "<< param->burnin << "\n";
+	for (int i = 0; i < param->burnin; i++){
+		state->update(r);
+	}
+	cout << "Sampling\n";
+	for (int i = 0; i < param->total; i++){
+		if (i % param->samp == 0){
+			//do something to sample the state
+		}
+		state->update(r);
+	}
+}
