0ef8023abbf4504ab9b4d621db3c9b015151f7d9
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-07-02 18:12:04 +0000

    Least squares updates--only one parameters for branches near root

--- trunk/test2.cpp
+++ trunk/test2.cpp
@@ -8,6 +8,7 @@
 #include "CountData.h"
 #include "PopGraph.h"
 #include "GraphState2.h"
+#include "PhyloPop_params.h"
 
 int main (void)
 {
@@ -19,17 +20,29 @@ int main (void)
 	r = gsl_rng_alloc(T);
 	gsl_rng_set(r, 100);
 
+	PhyloPop_params p;
+	//p.bias_correct = false;
 	CountData tmpcount("testin_counts.gz", 1);
-	tmpcount.read_alfreqs("testin_freqs.gz");
+	tmpcount.read_alfreqs("testin_freqs3.gz");
 	tmpcount.print_cov("test_cov.gz");
-	GraphState2 state(&tmpcount);
+	GraphState2 state(&tmpcount, &p);
 	//state.print_sigma();
-	//state.add_pop();
-	//state.add_pop();
-	//state.add_pop();
+	state.add_pop();
+	state.add_pop();
+	state.add_pop();
 	//cout << state.tree->get_newick_format() <<"\n";
 
+	//state.tree->print();
+	//set<Graph::vertex_descriptor> tset = state.tree->get_root_adj();
+	//for (set<Graph::vertex_descriptor>::iterator it = tset.begin(); it != tset.end(); it++){
+	//	cout << state.tree->g[*it].index << "\n";
+	//}
 
+	//state.set_graph("(((((pop0:0.1,pop1:0.1):0.1,pop2:0.1):0.1,pop3:0.1):0.1,pop4:0.1):0.1,pop5:0.1);");
+	//state.set_graph("((pop0:0.1,(pop1:0.1,pop2:0.1):0.1):0.1,(pop5:0.1,(pop3:0.1,pop4:0.1):0.1));");
+	//state.tree->print();
+	//state.set_branches_ls();
+	cout << state.tree->get_newick_format() <<"\n";
 	/*
 	vector<string> tmpnames;
 	tmpnames.push_back("pop1"); tmpnames.push_back("pop2");tmpnames.push_back("pop3");
