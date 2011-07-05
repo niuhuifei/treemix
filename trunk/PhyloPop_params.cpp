ffdffc0f6ef941698e039116194caff2d960d31e
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-07-05 21:49:26 +0000

    parameter object

---
+++ trunk/PhyloPop_params.cpp
@@ -0,0 +1,12 @@
+/*
+ * PhyloPop_params.cpp
+ *
+ *  Created on: Jul 1, 2011
+ *      Author: pickrell
+ */
+
+#include "PhyloPop_params.h"
+
+PhyloPop_params::PhyloPop_params(){
+	bias_correct = true;
+}
