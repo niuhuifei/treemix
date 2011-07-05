91a4721db8c088e715b7a1f11b24eb790a6a6fd0
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-07-05 21:49:26 +0000

    parameter object

---
+++ PhyloPop_params.h
@@ -0,0 +1,19 @@
+/*
+ * PhyloPop_params.h
+ *
+ *  Created on: Jul 1, 2011
+ *      Author: pickrell
+ */
+
+#ifndef PHYLOPOP_PARAMS_H_
+#define PHYLOPOP_PARAMS_H_
+
+#include "Settings.hpp"
+
+class PhyloPop_params{
+public:
+	PhyloPop_params();
+	bool bias_correct;
+};
+
+#endif /* PHYLOPOP_PARAMS_H_ */
