c6c23974cff0597d880ede37983bf59b0ce100f2
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-07-30 00:11:03 +0000

    Methods for working with graph

--- trunk/test2.cpp
+++ trunk/test2.cpp
@@ -22,7 +22,7 @@ int main (void)
 
 	PhyloPop_params p;
 	//p.bias_correct = false;
-	CountData tmpcount("testin.gz", 1);
+	CountData tmpcount("testin_counts.gz", 1);
 	GraphState2 state(&tmpcount, &p);
 	//state.print_sigma();
 	state.add_pop();
@@ -34,18 +34,23 @@ int main (void)
 	cout << state.tree->get_newick_format() <<"\n";
 	vector<Graph::vertex_descriptor> inorder = state.tree->get_inorder_traversal(4);
 	state.tree->print();
+	cout << state.tree->does_mig_exist(inorder[1], inorder[4]) <<"\n";
+	cout << "legal? "<< state.tree->is_legal_migration(inorder[1], inorder[4]) <<"\n";
 	state.tree->add_mig_edge(inorder[1], inorder[4]);
+	cout << state.tree->does_mig_exist(inorder[1], inorder[4]) <<"\n";
+	cout << "legal? "<< state.tree->is_legal_migration(inorder[4], inorder[1]) <<"\n";
+	state.tree->add_mig_edge(inorder[4], inorder[1]);
 	state.tree->print();
-	cout <<"here\n";
+
 	set< pair<double, set<Graph::vertex_descriptor> > > tmppath = state.tree->get_paths_to_root(inorder[4]);
-	cout << "size "<< tmppath.size() <<"\n";
-	for( set< pair<double, set<Graph::vertex_descriptor> > >::iterator it = tmppath.begin(); it != tmppath.end(); it++){
-		cout << it->first << " t\n"; cout.flush();
-		for (set<Graph::vertex_descriptor>::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++){
-			cout << state.tree->g[*it2].index <<"\n";
-		}
-		cout << "\n";
-	}
+	//cout << "size "<< tmppath.size() <<"\n";
+	//for( set< pair<double, set<Graph::vertex_descriptor> > >::iterator it = tmppath.begin(); it != tmppath.end(); it++){
+	//	cout << it->first << " t\n"; cout.flush();
+	//	for (set<Graph::vertex_descriptor>::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++){
+	//		cout << state.tree->g[*it2].index <<"\n";
+	//	}
+	//	cout << "\n";
+	//}
 
 
 	//state.set_graph("(((((pop0:0.1,pop1:0.1):0.1,pop2:0.1):0.1,pop3:0.1):0.1,pop4:0.1):0.1,pop5:0.1);");
