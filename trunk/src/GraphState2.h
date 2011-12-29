0931033427d087aa4164b02cdd99380602b51792
joepickrell@gmail.com <joepickrell@gmail.com@948fc884-838c-d1ed-5f51-c11e1ff4cf31>
2011-12-29 00:36:45 +0000

    Bug fix in CountData: fixed handling of missing data (now ignore, instead of setting to 0)

--- trunk/src/GraphState2.h
+++ trunk/src/GraphState2.h
@@ -106,6 +106,7 @@ public:
 	int golden_section_weight(Graph::edge_descriptor, double, double, double, double, int*);
 	int golden_section_weight_quick(Graph::edge_descriptor, double, double, double, double, int*);
 	int golden_section_weight_noexp(Graph::edge_descriptor, double, double, double, double, int*);
+	int golden_section_weight_noexp_quick(Graph::edge_descriptor, double, double, double, double, int*);
 	int golden_section_weight_wish(Graph::edge_descriptor, double, double, double, double);
 	void optimize_fracs();
 	void optimize_fracs(Graph::edge_descriptor);
